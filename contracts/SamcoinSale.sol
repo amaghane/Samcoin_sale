b13c58c5667758148c71f9a25c379f3f59bbbee2
amaghane <amaghane@gmail.com>
2021-05-31 02:51:55 +0200

    initial

---
+++ contracts/SamcoinSale.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import "./Samcoin.sol";
+
+contract SamcoinSale {
+
+    address payable admin;
+    Samcoin public tokenContract;
+    uint256 public tokenPrice;
+    uint256 public tokenSold;
+
+    event Sell (
+        address _buyer,
+        uint256 _amount
+    );
+
+    constructor(Samcoin _tokenContract, uint256 _tokenPrice) public {
+
+        admin = msg.sender;
+        tokenContract = _tokenContract;
+        tokenPrice = _tokenPrice;
+
+    }
+
+    function multiply (uint x, uint y) internal pure returns (uint z) {
+        require (y == 0 || (z = x * y) / y == x);
+    }
+
+    function buyTokens(uint256 _numberOfTokens) public payable {
+
+        require(msg.value == multiply(_numberOfTokens, tokenPrice));
+        require(tokenContract.balanceOf(address(this)) >= _numberOfTokens);
+        require(tokenContract.transfer(msg.sender, _numberOfTokens));
+
+        tokenSold += _numberOfTokens;
+
+        emit Sell(msg.sender, _numberOfTokens);
+
+    }
+
+    function endSale() public {
+        require(msg.sender == admin);
+        tokenContract.transfer(admin, tokenContract.balanceOf(address(this)));
+        selfdestruct(address(admin));
+
+    }
+}
